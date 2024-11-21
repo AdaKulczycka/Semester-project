6113d4f66e0475516291f46e9a9b2073613d43cd
Vlad <vlad.tomulescu317@gmail.com>
2024-11-21 22:22:46 +0100

    colorful text

---
+++ WasteHunters/WorldOfZuul/CommandWords.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WasteHunters
+{
+    public class CommandWords
+    {
+        public List<string> ValidCommands { get; } = new List<string> { "north", "east", "south", "west", "look", "back", "quit", "out", "help", "take", "trash", "inventory", "map","remove","compost"};
+
+        public bool IsValidCommand(string command)
+        {
+            return ValidCommands.Contains(command);
+        }
+    }
+
+}
---
+++ WasteHunters/WorldOfZuul/Item.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+
+namespace WasteHunters{
+    public class Inventory{
+        int sum = 0; 
+
+        private Dictionary<string, Dictionary<string, int>> items;
+        public Inventory()
+        {
+             items = new Dictionary<string, Dictionary<string, int>>()
+            {
+                { "plastic", new Dictionary<string, int>() },
+                { "paper", new Dictionary<string, int>() },
+                { "glass", new Dictionary<string, int>() },
+                { "bio waste", new Dictionary<string, int>() }
+            };
+        }
+
+        public void AddItemToPlastic(string itemName, int value)
+        {
+            AddItemToCategory("plastic", itemName, value);
+        }
+
+        public void AddItemToPaper(string itemName, int value)
+        {
+            AddItemToCategory("paper", itemName, value);
+        }
+
+        public void AddItemToGlass(string itemName, int value)
+        {
+            AddItemToCategory("glass", itemName, value);
+        }
+        public void AddItemToBioWaste(string itemName, int value)
+        {
+            AddItemToCategory("bio waste", itemName, value);
+        }
+        // Generic method to add items to any category
+        private void AddItemToCategory(string category, string itemName, int value)
+        {
+            items[category][itemName] = value; // Add new item to category
+        }
+
+        // Method to display the contents of each category
+        public void ShowInventory()
+        {
+            Console.WriteLine("Inventory:");
+
+            foreach (var category in items.Keys)
+            {
+                Console.WriteLine($"\n{category.ToUpper()}:");
+                if (items[category].Count == 0)
+                {
+                    Console.WriteLine("  (Empty)");
+                }
+                else
+                {
+                    foreach (var item in items[category])
+                    {
+                        Console.WriteLine($"  - {item.Key}: {item.Value} points");
+                    }
+                }
+            }
+        }
+        public void RemoveItem(string category, string itemName)
+        {
+            if (category == "bio waste"){
+                items["bio waste"].Clear();
+            }
+
+            if (items[category].ContainsKey(itemName) && category != "bio waste")
+            {
+                items[category].Remove(itemName);
+            }
+
+        }
+        public void CountPoints()
+        {
+            foreach (var category in items.Keys)
+            {
+                foreach (var item in items[category])
+                {
+                    sum += item.Value;
+                }
+            }
+            Console.WriteLine($"Sum up of all the values together: {sum}");
+        }
+    }
+}
